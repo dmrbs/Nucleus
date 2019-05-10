4a42740eccb306fed97abc7c178792cf817a3483
Kaan Demirbaş <kaan.dmrbs95@gmail.com>
2019-05-10 09:13:29 +0300

    measurement list added

---
+++ src/Nucleus.Web.Vue/src/admin/views/measurements/measurement-list.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <v-toolbar flat color="white">
+            <v-toolbar-title>{{$t('Measurements')}}</v-toolbar-title>
+            <v-divider class="mx-2"
+                       inset
+                       vertical>
+            </v-divider>
+            <v-spacer></v-spacer>
+            <v-text-field v-model="search"
+                          append-icon="search"
+                          :label="$t('Search')"
+                          single-line
+                          hide-details>
+            </v-text-field>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+
+        <v-data-table :headers="headers"
+                      :items="pagedListOfUserListDto.items"
+                      :pagination.sync="pagination"
+                      :total-items="pagedListOfUserListDto.totalCount"
+                      :loading="loading"
+                      class="elevation-1">
+            <template slot="items" slot-scope="props">
+                <td>deneme</td>
+                <td>deneme</td>
+                <td>deneme</td>
+                <td>deneme</td>
+                <td>deneme</td>
+                <td>deneme</td>
+
+                <td class="justify-center layout px-0">
+                    <v-icon v-if="nucleus.auth.isGranted('Permissions_User_Update')" small
+                            class="mr-2"
+                            @click="editUser(props.item.id)">
+                        edit
+                    </v-icon>
+                    <v-icon v-if="nucleus.auth.isGranted('Permissions_User_Delete')" small
+                            @click="deleteUser(props.item.id)">
+                        delete
+                    </v-icon>
+                </td>
+            </template>
+            <template slot="no-data" v-if="!loading">
+                <v-alert :value="true" color="error" icon="warning">
+                    {{$t('NothingToDisplay')}}
+                </v-alert>
+            </template>
+        </v-data-table>
+    </div>
+</template>
+
+<script src="./measurement-list.ts"></script>
