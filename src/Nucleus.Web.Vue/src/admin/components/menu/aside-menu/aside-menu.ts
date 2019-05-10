d4d6bb964624491e29e16b168dc9f8cce90ced8e
Kaan Demirbaş <kaan.dmrbs95@gmail.com>
2019-05-10 09:15:23 +0300

    updates were made.

--- src/Nucleus.Web.Vue/src/admin/components/menu/aside-menu/aside-menu.ts
+++ src/Nucleus.Web.Vue/src/admin/components/menu/aside-menu/aside-menu.ts
@@ -1,28 +1,30 @@
-﻿import NucleusComponentBase from '@/shared/application/nucleus-component-base';
-import { Component } from 'vue-property-decorator';
+﻿import NucleusComponentBase from "@/shared/application/nucleus-component-base";
+import { Component } from "vue-property-decorator";
 
 @Component
 export default class AsideMenuComponent extends NucleusComponentBase {
-    public drawer = true;
-    public isAdmin = false;
+  public drawer = true;
+  public isAdmin = false;
 
-    get mainMenuItems() {
-        return [
-            { icon: 'home', text: this.$t('Home'), link: '/admin/home' }
-        ];
-    }
+  get mainMenuItems() {
+    return [{ icon: "home", text: this.$t("Home"), link: "/admin/home" }];
+  }
 
-    get adminMenuItems() {
-        return [
-            { icon: 'people', text: this.$t('Users'), link: '/admin/user-list' },
-            { icon: 'work', text: this.$t('Roles'), link: '/admin/role-list' }
-        ];
-    }
+  get adminMenuItems() {
+    return [
+      { icon: "person", text: this.$t("Users"), link: "/admin/user-list" },
+      { icon: "work", text: this.$t("Roles"), link: "/admin/role-list" },
+      { icon: "group", text: this.$t("Groups"), link: "/admin/group-list" },
+      { icon: "info", text: this.$t("Positions"), link: "/admin/position-list" },
+      { icon: "tag", text: this.$t("Tags"), link: "/admin/tag-list" },
+      { icon: "expand", text: this.$t("Measurements"), link: "/admin/measurement-list" }
+      
+    ];
+  }
 
-    public mounted() {
-        this.$root.$on('drawerChanged',
-            () => {
-                this.drawer = !this.drawer;
-            });
-    }
+  public mounted() {
+    this.$root.$on("drawerChanged", () => {
+      this.drawer = !this.drawer;
+    });
+  }
 }
--- src/Nucleus.Web.Vue/src/router.ts
+++ src/Nucleus.Web.Vue/src/router.ts
@@ -25,7 +25,12 @@ const router = new VueRouter({
             children: [
                 { path: 'home', component: require('@/admin/views/home/home.vue').default },
                 { path: 'user-list', component: require('@/admin/views/users/user-list.vue').default },
-                { path: 'role-list', component: require('@/admin/views/roles/role-list.vue').default }
+                { path: 'role-list', component: require('@/admin/views/roles/role-list.vue').default },
+                { path: 'group-list', component: require('@/admin/views/groups/group-list.vue').default },
+                { path: 'position-list', component: require('@/admin/views/positions/position-list.vue').default },
+                { path: 'tag-list', component: require('@/admin/views/tags/tag-list.vue').default },
+                { path: 'measurement-list', component: require('@/admin/views/measurements/measurement-list.vue').default }
+
             ]
         }
     ]
